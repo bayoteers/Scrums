f0191ecd1765f8a91ed7ad37595a9d2d23dabdf7
Stephen Jayna <ext-stephen.jayna@nokia.com>
2011-04-20 12:46:29 +0100

    Result of running perltidy.

--- lib/Bugrpclib.pm
+++ lib/Bugrpclib.pm
@@ -50,12 +50,12 @@ sub update_bug_fields_from_json {
     my ($vars) = @_;
 
     my $cgi    = Bugzilla->cgi;
-    my $action = $cgi->param('action'); # Future use
-    my $data       = $cgi->param('data');
+    my $action = $cgi->param('action');    # Future use
+    my $data   = $cgi->param('data');
 
     my $json = new JSON::XS;
     if ($data =~ /(.*)/) {
-        $data = $1;    # $data now untainted
+        $data = $1;                        # $data now untainted
     }
     my $content = $json->allow_nonref->utf8->relaxed->decode($data);
 
@@ -65,9 +65,9 @@ sub update_bug_fields_from_json {
     $bug_obj_array_ref = $params->{ids};
     my @bug_obj_array = @{$bug_obj_array_ref};
     for my $bug_obj (@bug_obj_array) {
-        my @this_bug_id = keys %{$bug_obj};
-        my $bug_id = $this_bug_id[0];
-        my $bug_fields = $bug_obj->{$bug_id};
+        my @this_bug_id     = keys %{$bug_obj};
+        my $bug_id          = $this_bug_id[0];
+        my $bug_fields      = $bug_obj->{$bug_id};
         my @bug_field_names = keys %{$bug_fields};
         for my $field_name (@bug_field_names) {
             $field_value = $bug_fields->{$field_name};
@@ -75,42 +75,39 @@ sub update_bug_fields_from_json {
         }
     }
 }
-    
+
 sub update_bug_field {
     my ($vars, $bug_id, $field_name, $field_value) = @_;
 
     my $bug = Bugzilla::Bug->new($bug_id);
     my $old_value;
-    if($field_name eq 'estimated_time') {
+    if ($field_name eq 'estimated_time') {
         $bug->set_estimated_time($field_value);
         $bug->update();
     }
-    elsif($field_name eq 'remaining_time') {
+    elsif ($field_name eq 'remaining_time') {
         $bug->set_remaining_time($field_value);
         $bug->update();
     }
-    elsif($field_name eq 'assigned_to') {
-        if(Bugzilla::User::login_to_id($field_value))
-        {
-            # Login name is ok    
+    elsif ($field_name eq 'assigned_to') {
+        if (Bugzilla::User::login_to_id($field_value)) {
+            # Login name is ok
             $bug->set_assigned_to($field_value);
             $bug->update();
         }
-        else
-        {
-            $vars->{errors} = "login_name: " . $field_value. " is not known to Bugzilla";
+        else {
+            $vars->{errors} = "login_name: " . $field_value . " is not known to Bugzilla";
         }
     }
-    elsif($field_name eq 'priority') {
+    elsif ($field_name eq 'priority') {
         $bug->set_priority($field_value);
         $bug->update();
     }
-    elsif($field_name eq 'bug_severity') {
+    elsif ($field_name eq 'bug_severity') {
         $bug->set_severity($field_value);
         $bug->update();
     }
-    else
-    {
+    else {
         $vars->{errors} = "Not able to save column " . $field_name;
     }
 }
