3068084a3eeb8460473d975dff2c357721e2b64e
Visa Korhonen <visa.korhonen@symbio.com>
2011-10-24 11:14:33 +0300

    Task #286798, refactored HTML template processing to be self documenting

--- web/js/scrums.js
+++ web/js/scrums.js
@@ -27,7 +27,7 @@ function toggle_scroll()
     });
 }
 
-function listObject(ul_id, h_id, id, name, li_tmpl, link_url, offset_step) 
+function listObject(ul_id, h_id, id, name, li_tmpl_function, link_url, offset_step) 
 {
     this.ul_id = ul_id;
     this.id = id;
@@ -46,7 +46,7 @@ function listObject(ul_id, h_id, id, name, li_tmpl, link_url, offset_step)
         this.offset_step = 99999; // default value
     }
     this.name = name;
-    this.li_tmpl = li_tmpl;
+    this.line_template_function = li_tmpl_function;
     this.show_columns = ['order', 'bug_id', 'points', 'summary'];
     this.show_priority = true;
     this.show_creation_date = false;
@@ -193,20 +193,23 @@ function switch_lists(ui, lists) {
     //rewrite to list
     $("#" + lists[to_i].ul_id).children('tr').each(function(position, elem)
     {
-        $(elem).replaceWith(create_bug_elem(lists[to_i], bug_positions[to_i][$(elem).attr('id')]));
+        var index = bug_positions[to_i][$(elem).attr('id')];
+        var bug = lists[to_i].list[index];
+        var counter = index + 1;
+        var li_html = lists[to_i].line_template_function(bug, counter, lists[to_i].show_columns);
+
+        $(elem).replaceWith(li_html);
     });
 
     //rewrite from-list
-    $("#" + lists[from_i].ul_id).children('tr').each(function(position, elem)
-    {
-        $(elem).replaceWith(create_bug_elem(lists[from_i], bug_positions[from_i][$(elem).attr('id')]));
-    });
-
-    // rewrite to-list
     if(from_i != to_i) {
-        $("#" + lists[to_i].ul_id).children('tr').each(function(position, elem)
+        $("#" + lists[from_i].ul_id).children('tr').each(function(position, elem)
         {
-            $(elem).replaceWith(create_bug_elem(lists[to_i], bug_positions[to_i][$(elem).attr('id')]));
+            index = bug_positions[from_i][$(elem).attr('id')];
+	    bug = lists[from_i].list[index];
+	    counter = index + 1;
+            var li_html = lists[from_i].line_template_function(bug, counter, lists[from_i].show_columns);
+            $(elem).replaceWith(li_html);
         });
     }
 
@@ -258,22 +261,149 @@ function bind_sortable_lists(lists) {
     }).disableSelection();
 }
 
-function create_bug_elem(list, position)
+function create_item_line_html(bug, counter, show_columns)
 {
-    var template;
-    if (list.li_tmpl) {
-        template = list.li_tmpl
+    var str = '';
+    if (bug[10]) {
+        str += '<tr id=\'' + bug[0][0] + '\'  class="scrums_limited_area" bug_order_nr=\'' + counter + '\'>';
     }
     else {
-        template = $("#TeamBugTmpl");
-    } 
+        str += '<tr id=\'' + bug[0][0] + '\' bug_order_nr=\'' + counter + '\'>';
+    }
+    if ($.inArray('order', show_columns) > -1) { 
+        str += '<td class=\'colF sortable\'> <span class=\'number\'>' + counter + '</span></td>';
+    }
+    if ($.inArray('bug_id', show_columns) > -1) {
+	str += '<td class=\'colD sortable\'>' + bug[0][1] + '</td>';
+    }
+    if ($.inArray('points', show_columns) > -1) { 
+	str += '<td class=\'colF sortable\'>' + bug[1] + '</td>';
+    }
+    if ($.inArray('status', show_columns) > -1) { 
+	str += '<td class=\'colC sortable\'>' + bug[2] + '</td>';
+    }
+    if ($.inArray('assigned', show_columns) > -1) {
+        str += '<td class=\'colA sortable\'>' + bug[3] + '</td>';
+    }
+    if ($.inArray('severity', show_columns) > -1) { 
+        str += '<td class=\'colA sortable\'> ' + bug[7] + '</td>';
+    }
+    if ($.inArray('summary', show_columns) > -1) {  
+	str += '<td title=\'' + bug[5] + '\' class=\'sortable\'> ' + bug[5] + '</td>';
+    }
+    if ($.inArray('creation_date', show_columns) > -1) {  
+	str += '<td class=\'sortable\'>' + bug[6] + '</td>';
+    }
+    str += '</tr>';
+    return str;
+}
 
-    return parseTemplate(template.html(),
-    {
-        bug: list.list[position],
-	counter: position+1,
-        show_columns: list.show_columns,
-    });
+function create_list_html(list)
+{
+   var html = '<h3 id="' + list.h_id + '">'
+   if(list.link_url)
+   {
+       html += '<a href="' + list.link_url + '">' + list.name + ' (<span id="items_' + list.id + '"></span>)</a>';
+   }
+   else
+   {
+       html += list.name;
+   }
+   html += '</h3>';
+   html +=    '<div class="content" >';
+   html +=      '<div class="scrollTableContainer" id="container">';
+   html +=        '<table class="dataTable" id="table' + list.ul_id + '">';
+   html +=          '<thead id="mythead">';
+   html +=            '<tr>';
+   if ($.inArray('order', list.show_columns) > -1) { 
+       html += '<th class="sortable">#</th>';
+   }
+   if ($.inArray('bug_id', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.bug_id + '</th>';
+   }
+   if ($.inArray('points', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.remaining_time + '</th>';
+   }
+   if ($.inArray('status', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.bug_status + '</th>';
+   }
+   if ($.inArray('assigned', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.assigned_to + '</th>';
+   }
+   if ($.inArray('severity', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.bug_severity + '</th>';
+   }
+   if ($.inArray('summary', list.show_columns) > -1) {
+       html += '<th class="sortable colF">' + field_descs.short_desc + '</th>';
+   }
+   if ($.inArray('creation_date', list.show_columns) > -1) {
+       html += '<th class="sortable">' + field_descs.creation_ts + '</th>';
+   }
+   html +=            '</tr>';
+   html +=          '</thead>';
+   html +=        '<tbody id="' + list.ul_id + '" class="connectedSortable sortable">';
+   html +=        '</tbody>';
+   html +=      '</table>';
+   html +=    '</div>';
+   html +=  '</div>';
+   return html;
+}
+
+function create_release_item_list_html(list) {
+  var html = '<h3 id="' + list.h_id + '">' + list.name + '</h3>';
+  html += '<div class="content">';
+  html +=   '<p>';
+  html +=     '<input type="button" value="Previous" OnClick=\'move_list_left("' + list.id + '");\'/>';
+  html +=     '<input type="button" value="Next" OnClick=\'move_list_right("' + list.id + '");\'/>';
+  html +=     'Show';
+  html +=     '<select id="' + list.id + '" onchange=\'select_step("' + list.id + '");\'>';
+  html +=       '<option>10</option>';
+  html +=       '<option selected="selected">20</option>';
+  html +=       '<option>50</option>';
+  html +=       '<option>All</option>';
+  html +=     '</select>';
+  html +=     '(<span id="items_' + list.id + '"></span>)';
+  html +=   '</p>';
+  html +=   '<table class="dataTable">';
+  html +=     '<thead>';
+  html +=       '<tr>';
+  html +=         '<th class="sortable">Priority</th>';
+  html += 	  '<th class="sortable" class="colF">' + field_descs.bug_id + '</th>';
+  html +=         '<th class="sortable" class="colF">' + field_descs.bug_status + '</th>';
+  html +=         '<th class="sortable" class="colF">' + field_descs.bug_severity + '</th>';
+  html +=         '<th class="sortable" class="colF">' + field_descs.short_desc + '</th>';
+  html +=         '<th class="sortable" class="colF">Team</th>';
+  html +=         '<th class="sortable" class="colF">Sprint</th>';
+  html +=       '</tr>';
+  html +=     '</thead>';
+  html +=     '<tbody id="' + list.ul_id + '" class="connectedSortable sortable">';
+  html +=     '</tbody>';
+  html +=   '</table>';
+  html += '</div>';
+  return html;
+}
+
+
+function create_release_item_line(bug, counter, show_columns)
+{
+    var html = '<tr id="' + bug[0][0] + '" bug_order_nr="' + counter + '">';
+    if ($.inArray('order', show_columns) > -1) {
+        html += '<td class="colF"><span class="number">' + counter + '</span></td>';
+    }
+    if ($.inArray('creation_date', show_columns) > -1) {
+        html += '<td class="sortable">' + bug[6] + '</td>';
+    }
+    html += '<td class="colD">' + bug[0][1] + '</td>';
+    html += '<td class="colC">' + bug[1] + '</td>';
+    html += '<td class="colD">' + bug[2] + '</td>';
+    html += '<td class="colA">' + bug[3] + '</td>';
+    if ($.inArray('severity', show_columns) > -1) {
+        html += '<td class="colA sortable">' + bug[7] + '</td>';
+    }
+    html += '<td class="colB"><a href="page.cgi?id=scrums/createteam.html&teamid=' + bug[6] + '">' + bug[4] + '</a></td>';
+    html += '<td><a href="page.cgi?id=scrums/sprintburndown.html&sprintid=' + bug[7] + '">' + bug[5] + '</a></td>';
+    html += '</tr>';
+    return html;
 }
 
 
@@ -310,15 +440,16 @@ function update_lists(bugs_list, move_pos)
 
 
     var html = '';
-    // TODO This needs to fixed in order to paging to function
     for (var i = bugs_list.offset; i < bugs_list.visible.length; i++) {
 
         if (i > bugs_list.offset + bugs_list.offset_step - 1) {
             break;
         }
 
-        var position = bugs_list.visible[i];
-        html += create_bug_elem(bugs_list, position);
+        var index = bugs_list.visible[i];
+        var bug = bugs_list.list[index];
+        var counter = index + 1;
+        html += bugs_list.line_template_function(bug, counter, bugs_list.show_columns);
     } // for
 
     if (html)
@@ -720,35 +851,3 @@ function save_lists(ordered_lists, unordered_list, schema, obj_id)
     }
 }
 
-// le template engine
-var _tmplCache = {}
-this.parseTemplate = function(str, data) {
-    /// <summary>                                                                                                           
-    /// Client side template parser that uses &lt;#= #&gt; and &lt;# code #&gt; expressions.                                
-    /// and # # code blocks for template expansion.                                                                         
-    /// NOTE: chokes on single quotes in the document in some situations                                                    
-    ///       use &amp;rsquo; for literals in text and avoid any single quote                                               
-    ///       attribute delimiters.                                                                                         
-    /// </summary>                                                                                                          
-    /// <param name="str" type="string">The text of the template to expand</param>                                          
-    /// <param name="data" type="var">                                                                                      
-    /// Any data that is to be merged. Pass an object and                                                                   
-    /// that object's properties are visible as variables.                                                                  
-    /// </param>                                                                                                            
-    /// <returns type="string" />                                                                                           
-    var err = "";
-    try {
-        var func = _tmplCache[str];
-        if (!func) {
-            var strFunc = "var p=[],print=function(){p.push.apply(p,arguments);};" + "with(obj){p.push('" + str.replace(/[\r\t\n]/g, " ").replace(/'(?=[^#]*#>)/g, "\t").split("'").join("\\'").split("\t").join("'").replace(/<#=(.+?)#>/g, "',$1,'").split("<#").join("');").split("#>").join("p.push('") + "');}return p.join('');";
-            //alert(strFunc);                                                                                               
-            func = new Function("obj", strFunc);
-            _tmplCache[str] = func;
-        }
-        return func(data);
-    } catch (e) {
-        err = e.message;
-    }
-    return "< # ERROR: " + err + " # >";
-    //return "< # ERROR: " + err.htmlEncode() + " # >";                                                                     
-}
