361564bab4602c7fdad1f9a166c7bf94bc748e5d
David Wilson <dw@botanicus.net>
2012-02-14 23:39:43 +0200

    Revert "Round up capacity estimates to 2 digits."
    This reverts commit 432707be1af79268cee83142cb1c82da355c4765.

--- web/js/SprintPage.js
+++ web/js/SprintPage.js
@@ -22,19 +22,6 @@
 
 
 /**
- * Round some float to some precision.
- * @param number
- *      The float.
- * @param digits
- *      Precision.
- */
-function roundNumber(number, digits) {
-    var multiple = Math.pow(10, digits);
-    return Math.round(number * multiple) / multiple;
-}
-
-
-/**
  * Callback (pointlessly) invoked by detect_unsaved_change(); expected to
  * somehow save unsaved changes during window destruction.
  */
@@ -524,9 +511,8 @@ var SprintView = {
             sprint_total_work += row[9];
         });
 
-        $('#capa').html(roundNumber(this.sprint.estimatedcapacity, 2));
-        $('#free').html(
-            roundNumber(this.sprint.estimatedcapacity - sprint_total_work, 2));
+        $('#capa').html(this.sprint.estimatedcapacity);
+        $('#free').html(this.sprint.estimatedcapacity - sprint_total_work);
         $('#done').html(sprint_done_work);
         $('#remaining').html(sprint_remaining_work);
     },
